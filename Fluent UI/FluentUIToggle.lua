66df38baf996cefb411b0a5143d0e0c9c186b7cf
RileyBeeRBLX <rileybeerblx3@gmail.com>
2024-06-13 17:29:13 +0800

    Update FluentUIToggle.lua

--- Fluent UI/FluentUIToggle.lua
+++ Fluent UI/FluentUIToggle.lua
@@ -59,7 +59,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Fluent " .. Fluent.Version,
+    Title = "Fluent",
     SubTitle = "by dawid",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
